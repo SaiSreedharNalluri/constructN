7290c700aa00559a6eba9b529a3070e122a2b2e4
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-01-05 18:21:36 +0530

    In project page Structure List Dropdown adding completed

--- pages/projects/[projectId]/project/index.tsx
+++ pages/projects/[projectId]/project/index.tsx
@@ -1,35 +1,56 @@
 import { GetServerSideProps } from 'next';
-import React from 'react';
-import { IJobs } from '../../../../models/IJobs';
-import { getjobsInfo } from '../../../../services/jobs';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import _ from 'lodash';
 import Header from '../../../../components/container/header';
 import ProjectDetails from '../../../../components/container/projectDetails';
-
+import { getStructure } from '../../../../services/structure';
+import { IStrature } from '../../../../models/IStrature';
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const jobResp: any = await getjobsInfo(
+  const stractureResp: any = await getStructure(
     context.query.projectId as string,
     context
   );
   return {
-    props: { jobs: jobResp.data.result },
+    props: { structures: stractureResp.data.result },
   };
 };
 
 interface IProps {
-  jobs: IJobs[];
+  structures: IStrature[];
 }
 
-const Index: React.FC<IProps> = ({ jobs }) => {
+const Index: React.FC<IProps> = ({ structures }) => {
   const router = useRouter();
-
+  const getStractureHierarchy = (e: any) => {
+    //console.log('vjhdkvnds', e.target.value);
+  };
   return (
     <React.Fragment>
       <div className="h-screen">
         <div>
           <Header />
-          <ProjectDetails></ProjectDetails>
+          <div>
+            <select
+              className="focus:outline-none"
+              onChange={(e) => {
+                getStractureHierarchy(e.target.value);
+              }}
+            >
+              {structures &&
+                structures.map((stractureData: any) => {
+                  return (
+                    <option
+                      key={stractureData._id}
+                      value={stractureData._id}
+                      selected={stractureData.parent === null}
+                    >
+                      {stractureData.name}
+                    </option>
+                  );
+                })}
+            </select>
+          </div>
+          <ProjectDetails />
         </div>
       </div>
     </React.Fragment>
--- services/structure.ts
+++ services/structure.ts
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-export const getStructure = async (projectId: string) => {
+export const getStructure = async (projectId: string, context: any) => {
+  return await axios.get(
+    `${process.env.NEXT_PUBLIC_HOST}/projects/${projectId}/structures`,
+    {
+      headers: authHeader.authCookieHeader(context),
+    }
+  );
+};
+export const getStructureHierarchy = async (projectId: string) => {
   return await axios.get(
     `${process.env.NEXT_PUBLIC_HOST}/projects/${projectId}/structures/hierarchy`,
     {
