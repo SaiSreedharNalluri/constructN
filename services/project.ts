d2f33b43eb148d64dcfa5e3af6f988e36c3fab10
shivram-conn <shivram@constructn.ai>
2023-01-20 01:23:46 +0530

    Left Menu Updates
    Left Menu Selector Fixed
    View Screen Fixed

--- services/project.ts
+++ services/project.ts
@@ -9,3 +9,12 @@ export const getProjects = async (context: any) => {
     throw error;
   }
 };
+export const getProjectDetails = async (context: any,projectId:string) => {
+  // try {
+  //   return await axios.get(`${process.env.API_BASE_URL}/projects/${projectId as string}`, {
+  //     headers: authHeader.authCookieHeader(context),
+  //   });
+  // } catch (error) {
+  //   throw error;
+  // }
+};
