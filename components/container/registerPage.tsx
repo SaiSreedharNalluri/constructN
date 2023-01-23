e86576b8b16da69e97aad6c3476abf677e3e626d
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-01-23 11:29:43 +0530

    registration page and component creation completed

---
+++ components/container/registerPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NextImage from '../core/Image';
+const RegisterPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div className=" w-full   ">
+        <NextImage
+          src="https://constructn-attachments.s3.ap-south-1.amazonaws.com/Login/login02.png"
+          className="h-screen w-screen"
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+export default RegisterPage;
---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RegisterPage from '../components/container/registerPage';
+
+const Register: React.FC = () => {
+  return (
+    <React.Fragment>
+      <RegisterPage />
+    </React.Fragment>
+  );
+};
+export default Register;
