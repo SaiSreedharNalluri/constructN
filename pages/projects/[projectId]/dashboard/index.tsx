c900f9b603c3cdf3fe3e378d5de236e6b4950118
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-01-25 16:48:32 +0530

    dashboard and schedule page changes completed

--- pages/projects/[projectId]/dashboard/index.tsx
+++ pages/projects/[projectId]/dashboard/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../../../components/container/header';
 import { useRouter } from 'next/router';
 import CollapsableMenu from '../../../../components/layout/collapsableMenu';
 import authHeader from '../../../../services/auth-header';
+import { Tabs, Tab, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 const Index: React.FC = () => {
   const router = useRouter();
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <React.Fragment>
       <div>
@@ -12,14 +15,37 @@ const Index: React.FC = () => {
           <Header />
           <div className="flex w-screen fixed">
             <div>
-              <CollapsableMenu onChangeData={() => { }} />
+              <CollapsableMenu onChangeData={() => {}} />
             </div>
             <div>
-              <iframe
-                className="w-95 h-93"
-                src={`https://dev.internal.constructn.ai/task-dashboard?projectId=${router.query.projectId as string
-                  }&token=${authHeader.getAuthToken()}`}
-              />
+              <Tabs
+                selectedIndex={tabIndex}
+                onSelect={(index) => setTabIndex(index)}
+              >
+                <Tabs>
+                  <TabList>
+                    <Tab>Dash Board</Tab>
+                    <Tab>Reports</Tab>
+                  </TabList>
+
+                  <TabPanel>
+                    <iframe
+                      className="w-95 h-93"
+                      src={`https://dev.internal.constructn.ai/task-dashboard?projectId=${
+                        router.query.projectId as string
+                      }&token=${authHeader.getAuthToken()}`}
+                    />
+                  </TabPanel>
+                  <TabPanel>
+                    <iframe
+                      className="w-95 h-93  "
+                      src={`https://dev.internal.constructn.ai/reports?projectId=${
+                        router.query.projectId as string
+                      }&token=${authHeader.getAuthToken()}`}
+                    />
+                  </TabPanel>
+                </Tabs>
+              </Tabs>
             </div>
           </div>
         </div>
--- pages/projects/[projectId]/schedule/index.tsx
+++ pages/projects/[projectId]/schedule/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../../../components/container/header';
 import { useRouter } from 'next/router';
 import CollapsableMenu from '../../../../components/layout/collapsableMenu';
 import authHeader from '../../../../services/auth-header';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 const Index: React.FC = () => {
   const router = useRouter();
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <React.Fragment>
       <div>
@@ -12,14 +15,37 @@ const Index: React.FC = () => {
           <Header />
           <div className="flex w-screen fixed">
             <div>
-              <CollapsableMenu onChangeData={() => { }} />
+              <CollapsableMenu onChangeData={() => {}} />
             </div>
             <div>
-              <iframe
-                className="w-95 h-93"
-                src={`https://dev.internal.constructn.ai/project-plan?projectId=${router.query.projectId as string
-                  }&token=${authHeader.getAuthToken()}`}
-              />
+              <Tabs
+                selectedIndex={tabIndex}
+                onSelect={(index) => setTabIndex(index)}
+              >
+                <Tabs>
+                  <TabList>
+                    <Tab>Schedule View</Tab>
+                    <Tab>Gantt View</Tab>
+                  </TabList>
+
+                  <TabPanel>
+                    <iframe
+                      className="w-95 h-93"
+                      src={`https://dev.internal.constructn.ai/project-plan?projectId=${
+                        router.query.projectId as string
+                      }&token=${authHeader.getAuthToken()}`}
+                    />
+                  </TabPanel>
+                  <TabPanel>
+                    <iframe
+                      className="w-95 h-93"
+                      src={`https://dev.internal.constructn.ai/gantt?projectId=${
+                        router.query.projectId as string
+                      }&token=${authHeader.getAuthToken()}`}
+                    />
+                  </TabPanel>
+                </Tabs>
+              </Tabs>
             </div>
           </div>
         </div>
