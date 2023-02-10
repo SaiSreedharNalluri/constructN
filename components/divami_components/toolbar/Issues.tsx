e2254e6af872760c3982a524f35b7f4121d1b41d
Kausar <kausar.shaik@sevya-it.com>
2023-02-10 21:43:07 +0530

    create issue changes

--- components/divami_components/toolbar/Issues.tsx
+++ components/divami_components/toolbar/Issues.tsx
@@ -16,6 +16,10 @@ import {
   IssuesSectionClipImg,
 } from "./ToolBarStyles";
 import { Drawer } from "@mui/material";
+import CreateIssue from "../create-issue/CreateIssue";
+import CustomDrawer from "../custom-drawer/custom-drawer";
+import { createIssue } from "../../../services/issue";
+import { toast } from "react-toastify";
 
 const StyledDrawer = styled(Drawer)`
   & .MuiPaper-root {
@@ -23,10 +27,66 @@ const StyledDrawer = styled(Drawer)`
   }
 `;
 
-const Issues = ({ rightMenuClickHandler }: any) => {
+const Issues = (props: any) => {
   const [openIssueList, setOpenIssueList] = useState(false);
+  const [openCreateIssue, setOpenCreateIssue] = useState(false);
   // console.log(openIssueList, 'openIssueList')
 
+  const handleCreateTask = (formData: any) => {
+    console.log(formData, "form data at home");
+    clickTaskSubmit(formData);
+  };
+  const clickTaskSubmit = (formData: any) => {
+    let userIdList: any[] = [];
+    const assignes = formData.filter((item: any) => item.id == "assignedTo")[0]
+      ?.selectedName;
+    if (assignes.length > 0) {
+      assignes.map((user: any) => {
+        userIdList.push(user.value);
+      });
+    }
+    let data: any = {};
+    data.structure = props.currentStructure?._id;
+    data.title = `${props.currentStructure?.name}_${data.date} `;
+    data.snapshot = props.currentSnapshot?._id;
+    data.status = "To Do";
+    data.context = props.contextInfo;
+    (data.type = formData.filter(
+      (item: any) => item.id == "issueType"
+    )[0]?.defaultValue),
+      (data.priority = formData.filter(
+        (item: any) => item.id == "issuePriority"
+      )[0]?.defaultValue),
+      (data.description = formData.filter(
+        (item: any) => item.id == "description"
+      )[0]?.defaultValue),
+      (data.assignees = userIdList),
+      (data.tags =
+        formData.filter((item: any) => item.id == "tag-suggestions")[0]
+          ?.chipString || []),
+      (data.startDate = formData.filter(
+        (item: any) => item.id == "start-date"
+      )[0]?.defaultValue);
+    data.dueDate = formData.filter(
+      (item: any) => item.id == "due-date"
+    )[0]?.defaultValue;
+    const projectId = formData.filter((item: any) => item.projectId)[0]
+      .projectId;
+    console.log("formData", data);
+    createIssue(projectId as string, data)
+      .then((response) => {
+        if (response.success === true) {
+          toast.success("Issue added sucessfully");
+          // handleTaskSubmit(formData);
+          console.log(formData);
+        }
+      })
+      .catch((error) => {
+        if (error.success === false) {
+          toast.error(error?.message);
+        }
+      });
+  };
   const handleViewList = () => {
     // setOpenIssueList()
   };
@@ -41,7 +101,10 @@ const Issues = ({ rightMenuClickHandler }: any) => {
             width={12}
             height={12}
             alt="Arrow"
-            onClick={rightMenuClickHandler}
+            // onClick={rightMenuClickHandler}
+            onClick={() => {
+              setOpenCreateIssue(true);
+            }}
           />{" "}
         </IssuesSectionPlusImg>
 
@@ -63,7 +126,7 @@ const Issues = ({ rightMenuClickHandler }: any) => {
             width={12}
             height={12}
             alt="Arrow"
-            onClick={rightMenuClickHandler}
+            onClick={props.rightMenuClickHandler}
           />{" "}
         </IssuesSectionClipImg>
       </IssueBox>
@@ -76,6 +139,18 @@ const Issues = ({ rightMenuClickHandler }: any) => {
         >
           <IssueListing />
         </StyledDrawer>
+      )}
+      {openCreateIssue && (
+        <CustomDrawer open>
+          <CreateIssue
+            handleCreateTask={handleCreateTask}
+            setOpenCreateTask={setOpenCreateIssue}
+            currentProject={props.currentProject}
+            currentSnapshot={props.currentSnapshot}
+            currentStructure={props.currentStructure}
+            contextInfo={props.contextInfo}
+          />
+        </CustomDrawer>
       )}
     </div>
   );
