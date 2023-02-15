785e69e6bbc2eaad887a8ae180d528d7b0f45a69
sivaRajanaDivami <siva@divami.com>
2023-02-15 19:00:27 +0530

    toolbar fix

--- components/divami_components/toolbar/ToolBarMenuWrapper.tsx
+++ components/divami_components/toolbar/ToolBarMenuWrapper.tsx
@@ -211,7 +211,7 @@ const ToolBarMenuWrapper: React.FC<any> = ({
   return (
     <SectionToolBar viewMode={viewMode}>
       <ToolbarContainer>
-        {viewMode !== "Reality" ? (
+        {/* {viewMode !== "Reality" ? (
           <Typebar
             rightMenuClickHandler={rightMenuClickHandler}
             myTypesList={myTypesList}
@@ -243,7 +243,32 @@ const ToolBarMenuWrapper: React.FC<any> = ({
           />
         ) : (
           <></>
-        )}
+        )} */}
+        <Typebar
+          rightMenuClickHandler={rightMenuClickHandler}
+          myTypesList={myTypesList}
+          typeChange={typeChange}
+          selectedValue={selectedType}
+          openList={openSelectTypes}
+          setOpenList={setOpenSelectTypes}
+          onListClick={() => {
+            setOpenSelectLayer(false);
+            setOpenSelectTypes(!openSelectTypes);
+          }}
+        />
+        <Layers
+          rightMenuClickHandler={rightMenuClickHandler}
+          myLayersList={myLayersList}
+          LayerChange={LayerChange}
+          selectedValue={selectedLayer}
+          openList={openSelectLayer}
+          setOpenList={setOpenSelectLayer}
+          onListClick={() => {
+            setOpenSelectTypes(false);
+            setOpenSelectLayer(!openSelectLayer);
+          }}
+          selectedLayersList={selectedLayersList}
+        />
         <Issues
           issuesList={issuesList}
           issueMenuClicked={issueMenuClicked}
