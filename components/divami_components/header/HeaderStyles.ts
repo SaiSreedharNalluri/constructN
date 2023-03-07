cf0262b78dfacd6d2394a7c11c6b18ed633abd6e
swathi <swathi@divami.com>
2023-03-07 23:35:52 +0530

    fix avatar icon width and height issue

--- components/divami_components/header/HeaderStyles.ts
+++ components/divami_components/header/HeaderStyles.ts
@@ -1,69 +1,72 @@
-import { styled } from '@mui/system';
-import { Theme } from '../../../styles/globalStyles';
-import Image from 'next/image';
+import { styled } from "@mui/system";
+import { Theme } from "../../../styles/globalStyles";
+import Image from "next/image";
 
-export const HeaderContainer = styled('div')({
-  minHeight: '60px',
-  boxShadow: '0px 2px 3px rgba(0, 0, 0, 0.3)',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
+export const HeaderContainer = styled("div")({
+  minHeight: "60px",
+  boxShadow: "0px 2px 3px rgba(0, 0, 0, 0.3)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
 });
 
-export const HeaderLeftPart = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
+export const HeaderLeftPart = styled("div")({
+  display: "flex",
+  alignItems: "center",
 });
 
-export const HeaderLogoImageContainer = styled('div')({
-  padding: '0px 0px 0px 20px',
-  cursor: 'pointer',
+export const HeaderLogoImageContainer = styled("div")({
+  padding: "0px 0px 0px 20px",
+  cursor: "pointer",
 });
 
-export const HeaderRightPart = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-around',
+export const HeaderRightPart = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around",
 });
 
-export const HeaderToggle = styled('div')({
-  display: 'flex',
-  boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.2)',
-  marginRight: '15px',
-  borderRadius: '4px',
-  border: '1px solid #F1742E',
+export const HeaderToggle = styled("div")({
+  display: "flex",
+  boxShadow: "0px 3px 8px rgba(0, 0, 0, 0.2)",
+  marginRight: "15px",
+  borderRadius: "4px",
+  border: "1px solid #F1742E",
 });
 
-export const HeaderToggleButtonOne = styled('button')((props: any) => ({
-  background: !props?.toggleStatus ? 'none' : '#F1742E',
+export const HeaderToggleButtonOne = styled("button")((props: any) => ({
+  background: !props?.toggleStatus ? "none" : "#F1742E",
   // border: !props?.toggleStatus ? `1px solid #FFFFFF` : "none",
-  cursor: 'pointer',
-  color: !props?.toggleStatus ? '#36415D' : '#FFFFFF',
-  padding: '6px 10px',
-  height: '30px',
-  display: 'flex',
-  alignItems: 'center',
+  cursor: "pointer",
+  color: !props?.toggleStatus ? "#36415D" : "#FFFFFF",
+  padding: "6px 10px",
+  height: "30px",
+  display: "flex",
+  alignItems: "center",
 })) as any;
 
-export const HeaderProfileImageContainer = styled('div')({
-  marginRight: '15px',
-  cursor: 'pointer',
+export const HeaderProfileImageContainer = styled("div")({
+  marginRight: "15px",
+  cursor: "pointer",
   // border: "1.5px solid #36415D",
   // borderRadius: "50%",
   // boxShadow: "0px 3px 8px rgba(0, 0, 0, 0.2)",
 });
 
 export const ProfileImgIcon = styled(Image)({
-  cursor: 'pointer',
+  cursor: "pointer",
+  width: "35px",
+  borderRadius: "50%",
+  height: "35px",
   // boxShadow: "0px 3px 8px rgba(0, 0, 0, 0.2)",
 });
 
-export const HeaderMenuImageContainer = styled('div')({
-  marginRight: '15px',
-  cursor: 'pointer',
+export const HeaderMenuImageContainer = styled("div")({
+  marginRight: "15px",
+  cursor: "pointer",
 });
 
-export const HeaderNotificationImageContainer = styled('div')({
-  marginRight: '15px',
-  cursor: 'pointer',
+export const HeaderNotificationImageContainer = styled("div")({
+  marginRight: "15px",
+  cursor: "pointer",
 });
