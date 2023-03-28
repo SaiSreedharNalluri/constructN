76b0da8fe97a17fb2c8d03b17cb3b1e528130560
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-03-28 17:23:06 +0530

    dashboard and report pages UI integration

--- pages/projects/[projectId]/dashboard/index.tsx
+++ pages/projects/[projectId]/dashboard/index.tsx
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
-import Header from "../../../../components/divami_components/header/Header";
-import { useRouter } from "next/router";
-import CollapsableMenu from "../../../../components/layout/collapsableMenu";
-import authHeader from "../../../../services/auth-header";
-import { Tabs, Tab, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
-import SidePanelMenu from "../../../../components/divami_components/side-panel/SidePanel";
+import React, { useEffect, useState } from 'react';
+import Header from '../../../../components/divami_components/header/Header';
+import { useRouter } from 'next/router';
+import { Tabs, Tab, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import SidePanelMenu from '../../../../components/divami_components/side-panel/SidePanel';
+import { getdashBoardUrls } from '../../../../services/s3Service';
 const Index: React.FC = () => {
   const router = useRouter();
   const [tabIndex, setTabIndex] = useState(0);
+  const [urlObj, setUrlObj] = useState<{
+    dashboard_url: string;
+    report_url: string;
+  }>();
+  useEffect(() => {
+    if (router.isReady) {
+      getdashBoardUrls(router.query.projectId as string)
+        .then((response) => {
+          console.log('response', response);
+          setUrlObj(response);
+        })
+        .catch((error) => {
+          setUrlObj(undefined);
+        });
+    }
+  }, [router.isReady, router.query.projectId]);
   return (
     <React.Fragment>
       <div>
@@ -16,14 +31,13 @@ const Index: React.FC = () => {
           <Header />
           <div className="flex w-screen fixed">
             <div>
-              {/* <CollapsableMenu onChangeData={() => {}} /> */}
               <SidePanelMenu onChangeData={() => {}} />
             </div>
             <div>
               <Tabs
                 selectedIndex={tabIndex}
                 onSelect={(index) => setTabIndex(index)}
-                style={{ marginLeft: "1px" }}
+                style={{ marginLeft: '1px' }}
               >
                 <Tabs>
                   <TabList>
@@ -32,20 +46,25 @@ const Index: React.FC = () => {
                   </TabList>
 
                   <TabPanel>
-                    <iframe
-                      className="w-95 h-93"
-                      src={`https://dev.internal.constructn.ai/task-dashboard?projectId=${
-                        router.query.projectId as string
-                      }&token=${authHeader.getAuthToken()}`}
-                    />
+                    {urlObj ? (
+                      <iframe
+                        className="w-95 h-93"
+                        src={urlObj?.dashboard_url}
+                      />
+                    ) : (
+                      <h1 className=" absolute  top-1/2 bg-opacity-50 left-1/3 rounded p-2  bg-gray-300 text-orange-400 ">
+                        There is no data avalible to load the dashboard
+                      </h1>
+                    )}
                   </TabPanel>
                   <TabPanel>
-                    <iframe
-                      className="w-95 h-93  "
-                      src={`https://dev.internal.constructn.ai/reports?projectId=${
-                        router.query.projectId as string
-                      }&token=${authHeader.getAuthToken()}`}
-                    />
+                    {urlObj ? (
+                      <iframe className="w-95 h-93" src={urlObj?.report_url} />
+                    ) : (
+                      <h1 className=" absolute  top-1/2 bg-opacity-50 left-1/3 rounded p-2  bg-gray-300 text-orange-400 ">
+                        There is no data avalible to load the report
+                      </h1>
+                    )}
                   </TabPanel>
                 </Tabs>
               </Tabs>
