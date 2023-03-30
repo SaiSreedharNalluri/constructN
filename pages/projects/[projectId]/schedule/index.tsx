031e83e6b66b0f7dc43496b5ef4642ce0208ec5a
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-03-30 11:38:18 +0530

    gantt WIP

--- pages/projects/[projectId]/schedule/index.tsx
+++ pages/projects/[projectId]/schedule/index.tsx
@@ -1,14 +1,35 @@
-import React, { useState } from "react";
-import Header from "../../../../components/divami_components/header/Header";
-import { useRouter } from "next/router";
-import CollapsableMenu from "../../../../components/layout/collapsableMenu";
-import authHeader from "../../../../services/auth-header";
-import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
-import SidePanelMenu from "../../../../components/divami_components/side-panel/SidePanel";
+import React, { useEffect, useState } from 'react';
+import Header from '../../../../components/divami_components/header/Header';
+import { useRouter } from 'next/router';
+import authHeader from '../../../../services/auth-header';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import SidePanelMenu from '../../../../components/divami_components/side-panel/SidePanel';
+import moment from 'moment';
+const GanttChart = require('jsgantt-improved');
 const Index: React.FC = () => {
   const router = useRouter();
   const [tabIndex, setTabIndex] = useState(0);
+  const [data, setData] = useState<any>();
+  useEffect(() => {
+    setData([
+      {
+        name: 'Task 1',
+        start: moment('2023-03-20'),
+        end: moment('2023-03-25'),
+      },
+      {
+        name: 'Task 2',
+        start: moment('2023-03-22'),
+        end: moment('2023-03-27'),
+      },
+      {
+        name: 'Task 3',
+        start: moment('2023-03-25'),
+        end: moment('2023-03-29'),
+      },
+    ]);
+  }, []);
   return (
     <React.Fragment>
       <div>
@@ -16,14 +37,13 @@ const Index: React.FC = () => {
           <Header />
           <div className="flex w-screen fixed">
             <div>
-              {/* <CollapsableMenu onChangeData={() => {}} /> */}
               <SidePanelMenu onChangeData={() => {}} />
             </div>
             <div>
               <Tabs
                 selectedIndex={tabIndex}
                 onSelect={(index) => setTabIndex(index)}
-                style={{ marginLeft: "1px" }}
+                style={{ marginLeft: '1px' }}
               >
                 <Tabs>
                   <TabList>
@@ -40,12 +60,15 @@ const Index: React.FC = () => {
                     />
                   </TabPanel>
                   <TabPanel>
-                    <iframe
+                    <div>
+                      <GanttChart tasks={data} />
+                    </div>
+                    {/* <iframe
                       className="w-95 h-93"
                       src={`https://dev.internal.constructn.ai/gantt?projectId=${
                         router.query.projectId as string
                       }&token=${authHeader.getAuthToken()}`}
-                    />
+                    /> */}
                   </TabPanel>
                 </Tabs>
               </Tabs>
