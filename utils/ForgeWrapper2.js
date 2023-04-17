cadc10901e32dfab8f187d7a3ffdec2a77e75855
KT <krishnateja.t@constructn.ai>
2023-04-17 16:01:46 +0530

    possible fix for datviz not loading

--- utils/ForgeWrapper2.js
+++ utils/ForgeWrapper2.js
@@ -602,6 +602,9 @@ export const ForgeViewerUtils = (function () {
     if (!_isModelLoaded && progress.percent == 100) {
       console.log("Inside model load progress: ", progress, state, model);
       _isModelLoaded = true;
+      if (loadLayersOnDataLoadCompletion()) {
+        loadLayers();
+      }
     }
 
     if (progress.percent > 5 && _context) {
@@ -626,6 +629,9 @@ export const ForgeViewerUtils = (function () {
   const onGeometryLoadedEvent = (parameter) => {
     // console.log("Inside Geometry Loaded Event: model: ", parameter.model);
     _isModelLoaded = true;
+    if (loadLayersOnDataLoadCompletion()) {
+      loadLayers();
+    }
   };
 
   const onModelUnLoadedEvent = (model) => {
