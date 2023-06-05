cf4292671d28a1e4cbb568331e62bf3cd0cb88bb
swathi <swathi@divami.com>
2023-06-05 13:45:24 +0530

    add new projects list

--- components/divami_components/side-panel/SidePanel.tsx
+++ components/divami_components/side-panel/SidePanel.tsx
@@ -141,11 +141,7 @@ const SidePanelMenu: React.FC<IProps> = ({ onChangeData }) => {
       case "sections":
         router.push(`/projects/${router.query.projectId as string}/sections`);
         break;
-      case "projectList":
-        router.push(
-          `/projects/${router.query.projectId as string}/projectList`
-        );
-        break;
+
       case "usersList":
         router.push(`/projects/${router.query.projectId as string}/usersList`);
         break;
