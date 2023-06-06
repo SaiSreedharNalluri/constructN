d089ac7d11d00667b66e3808b191f961345ec1a2
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-06-06 16:59:30 +0530

    edit project ui integration completed

--- pages/projects/[projectId]/settings/index.tsx
+++ pages/projects/[projectId]/settings/index.tsx
@@ -1,47 +1,28 @@
-
-
-
-import React, { useEffect, useState } from 'react'
+import React from "react";
 import Header from "../../../../components/divami_components/header/Header";
-import SidePanelMenu from '../../../../components/divami_components/side-panel/SidePanel';
-import { IProjects } from '../../../../models/IProjects';
-import ProjectDetails from '../../../../components/container/projectDetails';
-import { getProjectDetails } from '../../../../services/project';
-import { useRouter } from "next/router";
+import SidePanelMenu from "../../../../components/divami_components/side-panel/SidePanel";
+import { IProjects } from "../../../../models/IProjects";
+import ProjectDetails from "../../../../components/container/projectDetails";
 
 interface IProps {
   projectData: IProjects;
 }
-const NewSettings:React.FC<IProps>=()=>{
-  const router = useRouter();
-  let [projectData, setProjectData] = useState<IProjects>();
-useEffect(()=>{
-if(router.isReady){
-  getProjectDetails(router.query.projectId as string)
-  .then((response) => {
-    if (response?.data?.success === true) {
-      setProjectData(response?.data?.result);
-    }
-  })
-  .catch();
-}
-},[router.isReady, router.query.projectId])  
+const Index: React.FC<IProps> = () => {
   return (
     <div>
       <div>
-      <Header></Header>
+        <Header></Header>
       </div>
-      <div className="flex w-full fixed" >
-      <div>
-        <SidePanelMenu onChangeData={() => {}} ></SidePanelMenu>
-      </div>
-      <div className="calc-w  calc-h overflow-y-auto ">
-      <ProjectDetails projectData={projectData as IProjects}></ProjectDetails>
-      </div>
+      <div className="flex w-full fixed">
+        <div>
+          <SidePanelMenu onChangeData={() => {}}></SidePanelMenu>
+        </div>
+        <div className="calc-w  calc-h overflow-y-auto ">
+          <ProjectDetails />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default NewSettings
+export default Index;
