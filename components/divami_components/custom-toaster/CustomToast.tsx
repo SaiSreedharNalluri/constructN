2d6212a6f14a8d2ed07fca71255795b2fd4aa14b
KT <krishnateja.t@constructn.ai>
2023-06-30 15:26:55 +0530

    customtoast accepts number and false for timer parameter stopped redirecting to sign in page in change password when incorrect password is entered

--- components/divami_components/custom-toaster/CustomToast.tsx
+++ components/divami_components/custom-toaster/CustomToast.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { toast } from "react-toastify";
 
-export const CustomToast = (message: string, type: string, timer?: number) => {
+export const CustomToast = (message: string, type: string, timer?: number | false) => {
   switch (type) {
     case "success":
       return toast.success(message, {
-        autoClose: timer ? timer : 5000,
+        autoClose: timer != undefined ? timer : 5000,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: true,
       });
     case "error":
       return toast.error(message, {
-        autoClose: timer ? timer : 5000,
+        autoClose: timer != undefined ? timer : 5000,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: true,
       });
     default:
       return toast(message, {
-        autoClose: timer ? timer : 5000,
+        autoClose: timer != undefined ? timer : 5000,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: true,
