949ba316ddb21f4c4f24325b8fd95ed7f20be73f
shivram-conn <shivram@constructn.ai>
2023-07-24 01:04:03 +0530

    iOS snapshot date change fixed
    date change was not reflecting in the TMC, fixed by adding new state

---
+++ utils/webgl-experiments.js
@@ -0,0 +1,29 @@
+// export default function webgl_experiments(){
+
+// }
+// export function getGLCanvas(ViewerId){
+//     let viewer_element=document.getElementById(ViewerId);
+// switch(ViewerId){
+//     case 'potreeViewer':
+//         console.log("Found Canvas in Potree",viewer_element.childNodes.length);
+//         return viewer_element.children[2]//.getContext("webgl");
+        
+//         break;
+//     case 'forgeViewer':
+//         canvas_element=document.getElementById(ViewerId).children[2]
+//         console.log("Found Canvas in Forge");
+//         break;
+//     case 'mapboxViewer':
+//         canvas_element=document.getElementById(ViewerId).children[2]
+//         console.log("Found Canvas in Mapbox");
+//         break;
+//     case 'minimapViewer':
+//         canvas_element=document.getElementById(ViewerId).children[2]
+//         console.log("Found Canvas in Minimap");
+//         break;
+//     default:
+//         console.log("Base Viewer Not Found");
+
+// }
+// return canvas_element;
+// }
