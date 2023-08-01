760dcc021d37cdf768b15d3574b6a9840e425b08
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-08-01 15:56:00 +0530

    code chnages in document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -150,7 +150,7 @@ export default function Document() {
         var g = document.getElementById("google_play");
         var a = document.getElementById("app_store");
 
-        if (userAgent.includes("iPhone")) {
+        if (userAgent.includes("iPhone")) 
             window.location.replace("https://apps.apple.com/us/app/constructn/id6449701613");
         }`}
         </Script>
