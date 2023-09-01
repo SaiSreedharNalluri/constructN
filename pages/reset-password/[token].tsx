156260fe026c89a85e3164cc58599460050fa2ae
SathyaKrishna Vanama <sathyakrishna.vanama@constructn.ai>
2023-09-01 16:59:46 +0530

    custom loader adding in reset Password page

--- pages/reset-password/[token].tsx
+++ pages/reset-password/[token].tsx
@@ -4,28 +4,35 @@ import { useRouter } from "next/router";
 import { CustomToast } from "../../components/divami_components/custom-toaster/CustomToast";
 import { validatePasswordToken } from "../../services/userAuth";
 import TokenNotVerify from "../../components/divami_components/reset-password/verificationLinkExpired/TokenNotVerifyFailed";
+import CustomLoader from "../../components/divami_components/custom_loader/CustomLoader";
 
 const ResetPasswordPage = () => {
   const router = useRouter();
   const [checkPage, setCheckPage] = useState<boolean>(false);
+  const[showLoading,setShowLoading] = useState(true)
   useEffect(() => {
     if (router.isReady) {
     validatePasswordToken(router.query.token as string)
         .then((response) => {
           if (response.success === true) {
-            CustomToast(response.message,"success");
             setCheckPage(true);
+            setShowLoading(false)
            }
         })
         .catch((error) => {
+          setShowLoading(false)
           CustomToast(error.message,"error");
         });
     }
   }, [router]);
   return (
     <div>
-     {checkPage ?
-      ( <ResetPassword />):(<div><TokenNotVerify/></div>)}
+      {
+      showLoading ?<CustomLoader/>:
+      (<div>{checkPage ?
+        ( <ResetPassword />):(<div><TokenNotVerify/></div>)}</div>)
+    }
+     
     </div>
   );
 };
