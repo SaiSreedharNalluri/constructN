6e810f806ea6ba5bcf3d23f55ec127c381e9eae2
shiva krishna <shiva.gopishetti@constructn.ai>
2023-10-03 16:11:43 +0530

    #4167 In 'Hierarchy' if there is 'No Design','No Capture' and 'Processing' then the background color of that block should be gray fixed that issue

--- components/divami_components/sectionsList/Chip.tsx
+++ components/divami_components/sectionsList/Chip.tsx
@@ -4,7 +4,7 @@ const Chips = (props:any) => {
   return (
     <div className={`text-center text-white  text-xs`}>
    
-      <p style={{ cursor:"default", width: `${isChip?"72px":"30px"}`, height:`${isChip?"":"20px"}`, wordWrap: 'break-word',backgroundColor:`${bgColor}`,padding:"4px",borderRadius:"4px" }}>
+      <p style={{ cursor:"default", display:"flex", alignItems:"center", justifyContent:"center", width: `${isChip?"72px":"30px"}`,marginRight:`${isChip?"":"10px"}`, height:`${isChip?"":"20px"}`, wordWrap: 'break-word',backgroundColor:`${bgColor}`,padding:"4px",borderRadius:"4px" }}>
           {title}
           </p>
     </div>
