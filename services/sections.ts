9962be8727ae731cdd67fe770c280feac99a55db
shiva krishna <shiva.gopishetti@constructn.ai>
2023-11-01 16:46:54 +0530

    #4333 "New" Chip UI Integrations in Sections Page and Project Hierarchy is Completed

--- services/sections.ts
+++ services/sections.ts
@@ -8,7 +8,24 @@ export const getSectionsList = async (projectId: string) => {
     }
   );
 };
-
+export const getNewChipData = async (projectId: string,t:string) => {
+  return await instance.get(
+    `${process.env.NEXT_PUBLIC_HOST}/projects/${projectId}/chips?type=${t}`,
+    {
+      headers: authHeader.authHeader(),
+    }
+  )
+};
+export const removeChip = async (projectId: string,chipId:string,structure:any) => {
+  console.log(projectId,chipId);
+  
+  return await instance.put(
+    `${process.env.NEXT_PUBLIC_HOST}/projects/${projectId}/chips/${chipId}`, {structure},
+    {
+      headers: authHeader.authHeader(),
+    }
+  )
+};
 // export const getStructureHierarchy = async (projectId: string) => {
 //   return await instance.get(
 //     `${process.env.NEXT_PUBLIC_HOST}/projects/${projectId}/structures/hierarchy`,
