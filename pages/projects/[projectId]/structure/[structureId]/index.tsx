c781e5592d839b702985ceaadb6ecb516493d70d
shivram-conn <shivram@constructn.ai>
2023-12-22 19:17:03 +0530

    Viewer BugFix #5013 #5014
    Time Line Component updated

--- pages/projects/[projectId]/structure/[structureId]/index.tsx
+++ pages/projects/[projectId]/structure/[structureId]/index.tsx
@@ -15,6 +15,7 @@ import { getGenViewerData } from "../../../../../services/genviewer";
 import TimeLineComponent from "../../../../../components/divami_components/timeline-container/TimeLineComponent";
 import { ISnapshot } from "../../../../../models/ISnapshot";
 import { CustomToast } from "../../../../../components/divami_components/custom-toaster/CustomToast";
+import { isMobile } from "../../../../../utils/ViewerDataUtils";
 
 const StructPage: React.FC = () => {
     //const [initData,setInintData] = useState<IGenData>(sampleGenData);
@@ -31,7 +32,7 @@ const StructPage: React.FC = () => {
     let [snapshotCompareList, setSnapshotCompareList] = useState<ISnapshot[]>([]);
     const [offset, setOffset] = useState(1);
     const [compareOffset, setCompareOffset] = useState(1);
-    const pageSize = 10;
+    const [pageSize,setPageSize] = useState(5);
     const [totalSnaphotsCount,setTotalSnaphotsCount] = useState(0);
     let [isFullScreenMode, setFullScreenMode] = useState(false);
   
@@ -112,10 +113,10 @@ const StructPage: React.FC = () => {
                 setInintData(response.result);
                 setBaseSnapshot(response.result.currentSnapshotBase);
                 setCompareSnapshot(response.result.currentSnapshotCompare);
-                setSnapshotList(response.result.snapshotList.slice(0,9).sort(
+                setSnapshotList(response.result.snapshotList.slice(0,(pageSize-1)).sort(
                   (a:ISnapshot, b:ISnapshot) => new Date(a.date).getTime() - new Date(b.date).getTime()
                 ));
-                setSnapshotCompareList(response.result.snapshotList.slice(0,9).sort(
+                setSnapshotCompareList(response.result.snapshotList.slice(0,(pageSize-1)).sort(
                   (a:ISnapshot, b:ISnapshot) => new Date(a.date).getTime() - new Date(b.date).getTime()
                 ));
                 setSnapshotListCal(response.result.snapshotList);
@@ -158,7 +159,7 @@ useEffect(()=>{
 },[initData]);
 
 useEffect(() => {
-  setTotalPages(Math.ceil(totalSnaphotsCount / 10));
+  setTotalPages(Math.ceil(totalSnaphotsCount / pageSize));
 }, [totalSnaphotsCount]);
 
 // const setPrevList = () => {
