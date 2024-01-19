21d10b1ee7c8749331b6e24e49e5cd8a37a430e5
shiva krishna <shiva.gopishetti@constructn.ai>
2024-01-19 15:56:00 +0530

    project details code changes

--- components/divami_components/edit-project/body/Body.tsx
+++ components/divami_components/edit-project/body/Body.tsx
@@ -23,9 +23,7 @@ const Body = ({ setCanBeDisabled, editData, handleFormData,setIsValidate,validat
   const [formState, setFormState] = useState({ selectedValue: "" });
   const [formConfig, setFormConfig] = useState(EDIT_PROJECT_FORM_CONFIG);
   const [projectTypes, setProjectTypes] = useState([]);
-  const router = useRouter();
-  console.log(editData,"edit data");
-  
+  const router = useRouter();  
   useEffect(() => {
     if (router.isReady) {
       getProjectTypes()
@@ -57,21 +55,22 @@ const Body = ({ setCanBeDisabled, editData, handleFormData,setIsValidate,validat
           if (item.id === "utm_value") {
             return {
               ...item,
-              defaultValue: editData?.utm || " ",
+              isDisabled: true,
+              defaultValue: editData?.utm?.zone || " ",
             };
           }
           if (item.id === "latitude") {
             return {
               ...item,
               isDisabled: true,
-              defaultValue: editData?.location?.coordinates[1] || " ",
+              defaultValue: editData?.location?.coordinates[1] || 0,
             };
           }
           if (item.id === "longitude") {
             return {
               ...item,
               isDisabled: true,
-              defaultValue: editData?.location?.coordinates[0] || " ",
+              defaultValue: editData?.location?.coordinates[0] || 0,
             };
           }
           if (item.id === "projectType") {
