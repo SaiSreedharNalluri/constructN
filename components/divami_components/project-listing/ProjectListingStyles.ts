1b63ede87044754541c24f30bb02debec2860dd4
sivalaxman007 <sivalaxman.mylapilli@constructn.ai>
2024-02-14 18:02:51 +0530

    increase height

--- components/divami_components/project-listing/ProjectListingStyles.ts
+++ components/divami_components/project-listing/ProjectListingStyles.ts
@@ -168,7 +168,7 @@ export const ProjectCardsContainer = styled("div")({
 export const ProjectCard = styled("div")((props: any) => ({
   width: props.active ? "300px" : "300px",
   // height: props.active ? "458px" : "438px",
-  height: "436px", //increasing height
+  height: "470px", //increasing height
   background: "#FFFFFF",
   border: "1px solid #888888",
   padding: "20px",
