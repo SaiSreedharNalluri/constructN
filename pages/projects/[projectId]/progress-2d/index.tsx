be2a908be8070e879b9e632f1ee55f620bb88e1e
sivalaxman007 <sivalaxman.mylapilli@constructn.ai>
2024-02-16 11:53:20 +0530

    categ

--- pages/projects/[projectId]/progress-2d/index.tsx
+++ pages/projects/[projectId]/progress-2d/index.tsx
@@ -271,6 +271,9 @@ const Progress2DPage: React.FC<any> = () => {
         fetchAssetCategories(projId!).then((response) => {
             if (response.data.result) {
                 const catSelected = response.data.result.find((cate: {_id: string})=>(cate._id === (selectedCategory ? selectedCategory :response.data.result[1])._id))
+                if(catSelected){
+                    _onCategorySelected(catSelected);
+                }
                 setAssetCategories(response.data.result);
                 _loadAssetsForCategory(catSelected);
             }
